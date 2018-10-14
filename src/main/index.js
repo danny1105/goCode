440d6a96eff93817b120628ab76c0720687e0305
ashish tiwari <ashishtiwari11@outlook.com>
2018-10-14 18:44:45 +0530

    Removed Express

--- src/main/index.js
+++ src/main/index.js
@@ -1,138 +1,77 @@
-const express = require('express');
-// const exphbs = require('express-handlebars');
-// const path = require('path');
-// const bodyParser = require('body-parser');
-// const methodOverride = require('method-override');
+//const express = require('express');
 const redis = require('redis');
-const Step = require('step');
+const mysql = require('mysql');
+const amqplib = require('amqplib/callback_api');
 
-//set port
-const port = 3000;
+//const app = express();
+const client = redis.createClient();
+//var msgtosql;
 
-//init app
-const app = express();
+amqplib.connect('amqp://localhost',function(err, conn) {
+        if(err){
+            throw err;
+        }
+        conn.createChannel(function(err, ch) {
+            if(err){
+                throw err;
+            }
+            var q = 'redis-assign';
+            ch.assertQueue(q, {durable: false});
+            ch.consume(q, function(msg) {
+                console.log('message recieved');
+                msgtosql = msg.content.toString();
+                mysql_insert(msgtosql);
+            }, {noAck: true});
+        });
+});
 
-// config file
-var config = require('config');
 
-var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var ex = 'logs';
+//create mysql db connection
+var db = mysql.createConnection({
+    host     : 'localhost',
+    port     : '',
+    user     : 'root',
+    password : 'ashish',
+    database : 'gocode'
+});
 
-    ch.assertExchange(ex, 'fanout', {durable: false});
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('Mysql connected...');
+});
 
-    ch.assertQueue('', {exclusive: true}, function(err, q) {
-      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-      ch.bindQueue(q.queue, ex, '');
-
-      ch.consume(q.queue, function(msg) {
-        console.log(" [x] %s", msg.content.toString());
-      }, {noAck: true});
+//app.get('/createdb', (req, res) => {
+    // let sql = 'CREATE TABLE goinfo(id int AUTO_INCREMENT, first VARCHAR(50), last VARCHAR(50), text VARCHAR(255), PRIMARY KEY (id))';
+    // db.query(sql, (err, result) => {
+    //          if(err){
+    //             throw err;
+    //         }
+    //         console.log(result);
+function mysql_insert(data) {
+    client.hgetall(data, (err, reply) => {
+        if(err){
+            throw err;
+        }
+        db.query('INSERT INTO gocode set ?', reply, (err, result) => {
+            if(err){
+             throw err;
+           }
+        console.log('MySQL table updated');
+        client.del(data, (err, reply) => {
+            if(err){
+                throw err;
+            }
+            console.log('Data deleted');
+        });
+        res.send('Everything is OK!!!');
     });
-  });
-});
-
-
-// mysql client connect
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-	host        	  : 'localhost',
-	port        	  : '',
-	database    	  : '',
-	user        	  : '',
-	password    	  : '',
-	insecureAuth	  : true,
-	multipleStatements: true
-});
-
-function handleDisconnect() {
-  connection.on('error', function(err) {  
-    if (!err.fatal) {
-      return;
-    }
-
-    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-      throw err;
-    }
-
-    connection = mysql.createConnection(connection.config);
-    handleDisconnect();
-    connection.connect();
+    //  });
   });
 }
-handleDisconnect();
 
-// redis client connect
-client = redis.createClient();
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
-
-var timeOut = 10;
-function dumpMysql() {
-
-	Step(
-		
-		function checkWorkingVars() {
-			client.multi()
-				.exists('data_tmp')
-				.exists('data')
-				.exec(this);
-		},
-		
-		function setWorkingVars(err, replies) {
-			existsTmp = replies[0];
-			existsData = replies[1];
-			
-			if(!existsData && !existsTmp) {
-				setTimeout(dumpMysql, timeOut*1000);
-			} else {
-				if(existsData && !existsTmp) {
-					client.rename('data', 'data_tmp');
-				}
-				client.smembers('data_tmp', this);
-			}
-		},
-		
-		function readData(err, data) {
-			for(var attr in data) {
-				connection.query('SELECT 1'); // db ping
-				
-				var query  = 'INSERT IGNORE INTO data (log) ';
-					query += 'VALUES ("'+data[attr]+'")';
-				connection.query(query);				
-			}
-			
-				
-		},
-				
-		function cleanUp(err, r) {
-			client.del('data_tmp', this);
-		},
-		
-		function restart(err, r) {
-			setTimeout(dumpMysql, timeOut*1000);
-		}
-		
-	);
-
-};
-setTimeout(dumpMysql, 1000);
-
-// stdout log started
-console.log('DUMP Mysql running...');
-
-app.get('/person/{id}', function(req, res, next){
-    res.render('query');
-});
-
-// app.delete('/peer/delete/:id', function(req, res, next){
-//     client.del(req.params.id);
-//     res.redirect('/');
-// })
-
-app.listen(port, function(){
-    console.log('server started on port '+port)
-});
+//app.listen('8080', () => {
+//    console.log('Server started on port 8080');
+//})
